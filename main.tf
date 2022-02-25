064d3d620e8837176927d9d8e76e4a17c6946f3e
Justin Kaufman <akaritakai@gmail.com>
2022-02-25 00:10:21 -0600

    Remove unnecessary provider

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.2.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
   }
   required_version = ">= 1.1.6"
   cloud {
