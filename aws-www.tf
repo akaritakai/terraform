0a94cfde9c23d512b4b98ded4ee87820904cd45a
Justin Kaufman <akaritakai@gmail.com>
2022-03-10 17:25:18 -0600

    I think the CSP might be a bit much

--- aws-www.tf
+++ aws-www.tf
@@ -41,16 +41,6 @@ function handler(event) {
   headers['cache-control'] = {
     value: 'max-age=86400, public'
   };
-  // Add a restrictive CSP for the site and a more generous one for the blog
-  if (event.request.uri.startsWith('/blog/')) {
-      headers['content-security-policy'] = {
-          value: "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self'; frame-src 'self' https://www.youtube-nocookie.com; img-src 'self' https://i.ytimg.com; manifest-src 'self'; media-src 'self'; worker-src 'none';"
-      }
-  } else {
-      headers['content-security-policy'] = {
-          value: "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self'; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; worker-src 'none';"
-      }
-  }
   return response;
 }
 EOD
