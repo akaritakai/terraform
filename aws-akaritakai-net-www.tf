a3d663c1ccdeea8b518190a638302244590ae08b
Justin Kaufman <akaritakai@gmail.com>
2022-03-10 00:26:50 -0600

    Add a redirect if no extension

--- aws-akaritakai-net-www.tf
+++ aws-akaritakai-net-www.tf
@@ -146,6 +146,8 @@ function handler(event) {
   var request = event.request;
   if (request.uri.endsWith('/')) {
     request.uri += 'index.html';
+  } else if (!request.uri.includes('.')) {
+    request.uri += '/index.html';
   }
   return request;
 }
